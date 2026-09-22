43e749ee80f5793ad9b3a8c9028030e33c222e66
pocketdown <junghunjoo@icloud.com>
2026-09-22 15:45:26 +0900

    Update imx219.py

--- imx219.py
+++ imx219.py
@@ -15,11 +15,11 @@ while True:
     dir = "/home/pi/test/" + datetime.now().strftime("%Y%m%d") + "/"
     subprocess.run(["mkdir", "-p", dir], stderr=subprocess.DEVNULL)
 
-    filename = dir + datetime.now().strftime("%Y%m%d") + "-" + gain + "-" + shutter + "-" + avg_brightness + ".jpg"
+    filename = dir + datetime.now().strftime("%Y%m%d-%H%M%S") + "-" + gain + "-" + shutter + "-" + avg_brightness + ".jpg"
     #print("file name: " + filename)
     subprocess.run(["rpicam-still", "-v", "0", "-o", filename, "-t", "5", "--gain", gain, "--shutter", shutter, "--width", width, "--height", height, "--framerate", "1", "--awb", "auto", "--metering", "spot", "--tuning-file", "/usr/share/libcamera/ipa/rpi/vc4/imx219_noir.json"])
 
-    files = glob(dir + str(years) + str(months).zfill(2) + str(days).zfill(2) + "-" + str(hours).zfill(2) + str(minutes).zfill(2) + "*.jpg")
+    files = glob(dir + datetime.now().strftime("%Y%m%d-%H%M%S") + "*.jpg")
     latest = max(files, key=os.path.getmtime)
     #print("latest name: " + latest)
     img = Image.open(latest).convert("L")
